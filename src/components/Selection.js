0d6dc4ca21f1dff03a23c14ad8af302f34c7670e
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-18 11:05:19 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import '../styles/Child.css';
 const Selection = ({applyColor}) =>{
   return (
     const [selectionStyle, updateSelectionStyle] = useState({});
