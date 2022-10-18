5f0c0db6c4375154ae435c2db55506db0220dc07
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-18 10:53:24 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,9 @@
-
+const Selection = ({applyColor}) =>{
+  return (
+    const [selectionStyle, updateSelectionStyle] = useState({});
+    <div className="fix-box" style={selectionStyle} onClick={()=>{applyColor(updateSelectionStyle)}}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  )
+}
+export default Selection;
