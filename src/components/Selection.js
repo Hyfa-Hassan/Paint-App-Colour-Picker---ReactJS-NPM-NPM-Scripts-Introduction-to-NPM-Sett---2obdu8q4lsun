2d2933d0291040f1d143fbcd8401f2454d769ed4
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-18 14:24:19 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,8 +1,8 @@
 import React, {useState} from "react";
 import '../styles/Child.css';
 const Selection = ({applyColor}) =>{
+const [selectionStyle, updateSelectionStyle] = useState({background:''});
   return (
-    const [selectionStyle, updateSelectionStyle] = useState({});
     <div className="fix-box" style={selectionStyle} onClick={()=>{applyColor(updateSelectionStyle)}}>
       <h2 className="subheading">Selection</h2>
     </div>
